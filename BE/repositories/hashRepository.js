b77d0bd6257e33335fa4dd3de6d078d9c89e63d8
Vibhav-Deo <45755558+Vibhav-Deo@users.noreply.github.com>
2022-08-15 10:40:24 +1000

    Update hashRepository.js

--- BE/repositories/hashRepository.js
+++ BE/repositories/hashRepository.js
@@ -20,6 +20,7 @@ const GET_RECORD = async (userFileHash) => {
 
 const INSERT_RECORD = async (userFileHash, userId) => {
   const USER_FILE_HASH_RECORD = await GET_RECORD(userFileHash);
+  console.log(USER_FILE_HASH_RECORD);
   if (USER_FILE_HASH_RECORD !== undefined) {
     throw new Error("Duplicate file being uploaded cannot insert file record");
   }
