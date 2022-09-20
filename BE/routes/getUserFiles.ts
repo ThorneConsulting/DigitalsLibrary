fbecc176e3c84560be2b19a8ab351949794d9a82
Vibhav Deo <vibhavdeo@Vibhavs-MacBook-Pro-2.local>
2022-09-20 19:03:24 +1000

    Added console.log

--- BE/routes/getUserFiles.ts
+++ BE/routes/getUserFiles.ts
@@ -13,6 +13,7 @@ export const getUserFiles = async (event: APIGatewayEvent) => {
     console.log("Found Item", RESULT);
 
     RESPONSE = await CREATE_RESPONSE(RESULT, "SUCCESS", 200);
+    console.log(RESPONSE);
   } catch (error: any) {
     console.error("Failed to get record");
 
