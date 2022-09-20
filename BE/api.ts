90369d117323f19eb033c67b534d2ba93336cf1a
Vibhav Deo <vibhavdeo@Vibhavs-MacBook-Pro-2.local>
2022-09-21 07:30:13 +1000

    Changing the import

--- BE/api.ts
+++ BE/api.ts
@@ -1,7 +1,11 @@
+import { APIGatewayEvent } from "aws-lambda";
 import * as ROUTES from "./routes";
-
+const getUserFiles = async (event: APIGatewayEvent) => {
+  const RESULT = await ROUTES.getUserFiles(event);
+  return RESULT;
+};
 module.exports = {
-  getUserFiles: ROUTES.getUserFiles,
+  getUserFiles,
   uploadUserFile: ROUTES.uploadUserFile,
   createUserBucketIfNotExist: ROUTES.createUserBucketIfNotExist,
 };
